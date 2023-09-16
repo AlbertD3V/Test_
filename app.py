56182406969322784b83c916d568a9524aa3a995
AlbertDias <albrtcd@gmail.com>
2023-09-16 12:02:40 -0300

    segundo commit

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 print('hello, I need to buy a coffee and cookies...')
 
 while True:
+    nome = str(input("What's your nome? "))
+    print(f"Hi {nome}, how are you? ")
+    resp = str(input("? "))
+    print(f"Oh, Ok..")
     
